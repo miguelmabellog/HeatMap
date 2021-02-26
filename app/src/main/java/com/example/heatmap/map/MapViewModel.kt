b0430fde79183550cfaeba69eb143d0a31130a1c
Miguel Bello <miguel.angel.bello.garcia123@gmail.com>
2021-02-26 17:19:51 -0500

    complete numbers

--- app/src/main/java/com/example/heatmap/map/MapViewModel.kt
+++ app/src/main/java/com/example/heatmap/map/MapViewModel.kt
@@ -41,7 +41,105 @@ class MapViewModel(application: Application): AndroidViewModel(application) {
     }
 
 
+    var _twoOne = MutableLiveData<String>()
 
+    val getTwoOne=Transformations.map(_twoOne){
+        if(it==""){
+            0F
+        }
+        else{
+            it.toFloat()
+        }
+    }
+
+    var _twoTwo = MutableLiveData<String>()
+
+    val getTwoTwo=Transformations.map(_twoTwo){
+        if(it==""){
+            0F
+        }
+        else{
+            it.toFloat()
+        }
+    }
+
+    var _twoThree = MutableLiveData<String>()
+
+    val getTwoThree=Transformations.map(_twoThree){
+        if(it==""){
+            0F
+        }
+        else{
+            it.toFloat()
+        }
+    }
+
+
+    var _threeOne = MutableLiveData<String>()
+
+    val getThreeOne=Transformations.map(_threeOne){
+        if(it==""){
+            0F
+        }
+        else{
+            it.toFloat()
+        }
+    }
+
+    var _threeTwo = MutableLiveData<String>()
+
+    val getThreeTwo=Transformations.map(_threeTwo){
+        if(it==""){
+            0F
+        }
+        else{
+            it.toFloat()
+        }
+    }
+
+    var _threeThree = MutableLiveData<String>()
+
+    val getThreeThree=Transformations.map(_threeThree){
+        if(it==""){
+            0F
+        }
+        else{
+            it.toFloat()
+        }
+    }
+
+    var _fourOne = MutableLiveData<String>()
+
+    val getFourOne=Transformations.map(_fourOne){
+        if(it==""){
+            0F
+        }
+        else{
+            it.toFloat()
+        }
+    }
+
+    var _fourTwo = MutableLiveData<String>()
+
+    val getFourTwo=Transformations.map(_fourTwo){
+        if(it==""){
+            0F
+        }
+        else{
+            it.toFloat()
+        }
+    }
+
+    var _fourThree = MutableLiveData<String>()
+
+    val getFourThree=Transformations.map(_fourThree){
+        if(it==""){
+            0F
+        }
+        else{
+            it.toFloat()
+        }
+    }
 
 
 
